c151ce0f6c60172c304177001c2853a7ee7f6dc2
Dener Rodrigues <dl.rodrigues94@gmail.com>
2023-10-10 17:14:23 -0300

    Implements web scraping

--- libreview/src/utils/web_scraping.py
+++ libreview/src/utils/web_scraping.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-from selenium.webdriver import Firefox
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
 
 from src import settings
 
@@ -7,15 +8,15 @@ from src import settings
 def login():
     api_token = None
 
-    browser = Firefox()
+    browser = Chrome()
 
     browser.get(settings.LOGIN_URL)
-    browser.find_element_by_id("loginForm-email-input").send_keys(settings.LOGIN_EMAIL)
-    browser.find_element_by_id("loginForm-password-input").send_keys(settings.LOGIN_PASSWORD)
-    browser.find_element_by_id("loginForm-submit-button").click()
+    browser.find_element(By.ID, "loginForm-email-input").send_keys(settings.LOGIN_EMAIL)
+    browser.find_element(By.ID, "loginForm-password-input").send_keys(settings.LOGIN_PASSWORD)
+    browser.find_element(By.ID, "loginForm-submit-button").click()
 
     browser.get(settings.LOGIN_SEND_CODE_URL)
-    browser.find_element_by_id("twoFactor-step1-next-button").click()
+    browser.find_element(By.ID, "twoFactor-step1-next-button").click()
 
     soup = BeautifulSoup(browser.page_source, "html.parser")
 
